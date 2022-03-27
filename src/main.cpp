e2a8ff0d1e6a56a6eaba24335ede10ff2fc1c212
Tima-bd <100956970+Tima-bd@users.noreply.github.com>
2022-03-27 23:25:45 +0300

    Balandin Adding main.cpp and automata.cpp

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+
+#include "automata.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    Automata automat;
+    automat.on();
+    cout << "automat is waiting" << endl;
+    automat.coin(5);
+    cout << "incerted 5 tugriks" << endl;
+    automat.coin(10);
+    cout << "incerted 10 tugriks" << endl;
+    automat.etMenu();
+    cout << "menu shown" << endl;
+    automat.choice("fried nails");
+    cout << "user selected fried nails" << endl;
+    automat.check();
+    cout << "automat checked cash" << endl;
+    automat.cook();
+    cout << "automat cooked nails" << endl;
+    automat.finish();
+    cout << "automat finished work" << endl;
+}
