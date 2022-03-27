051e790228da3a3554927745726933f136f099b7
Tima-bd <100956970+Tima-bd@users.noreply.github.com>
2022-03-27 23:26:33 +0300

    Balandin update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,28 @@
 // Copyright 2022 GHA Test Team
 
 #include <gtest/gtest.h>
-#include "Automata.h"
+#include "automata.h"
+
+TEST(task1, operation)
+{
+	Automata coffee;
+	coffee.state = OFF;
+	coffee.on();
+	States ok = coffee.get_state();
+	EXPECT_EQ(Wait, ok);
+}
+TEST(task2, operation)
+{
+	Automata coffee;
+	coffee.state = COOK;
+	coffee.on();
+	States ok = coffee.get_state();
+	EXPECT_EQ(COOK, ok);
+}
+TEST(task3, operation)
+{
+	Automata coffee;
+	coffee.state = CHECK;
+	coffee.check();
+	EXPECT_EQ(isEnough, 1);
+}
