ae05713ee5a7b3a7a19afbe125439d74abf1e61a
Tima-bd <100956970+Tima-bd@users.noreply.github.com>
2022-03-28 16:13:49 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,99 +1,77 @@
-
+// Copyright 2022 UNN-IASR
+#pragma once
 #include "Automata.h"
+using std::cout;
+using std::endl;
 Automata::Automata() {
     state = OFF;
     cash = 0;
     int menuLen = 6;
     bool isEnough = 0;
 }
-void Automata::on()
-{
+void Automata::on() {
     if (state == OFF)
         state = WAIT;
     else
         cout << "Error" << endl;
 }
-void Automata::off()
-{
+void Automata::off() {
     if (state == WAIT)
         state = OFF;
     else
         cout << "Error" << endl;
 }
-void Automata::coin(int money)
-{
-    if (state == WAIT || state == ACCEPT)
-    {
+void Automata::coin(int money) {
+    if (state == WAIT || state == ACCEPT) {
         state = ACCEPT;
         cash += money;
-    }
-    else
+    } else
         cout << "Error" << endl;
 }
-void Automata::etMenu()
-{
-    for (int i = 0; i < menuLen; i++)
-    {
+void Automata::etMenu() {
+    for (int i = 0; i < menuLen; i++) {
         cout << menu[i] << "=" << prices[i] << endl;
     }
 }
-STATES Automata::getState()
-{
+STATES Automata::getState() {
     return state;
 }
-void Automata::choice(std::string drink)
-{
-    if (state == ACCEPT)
-    {
+void Automata::choice(std::string drink) {
+    if (state == ACCEPT) {
         choseDrink = drink;
         state = CHECK;
-    }
-    else
+    } else
         cout << "Error" << endl;
 }
-void Automata::check()
-{
-    if (state == CHECK)
-    {
+void Automata::check() {
+    if (state == CHECK) {
         for (int i = 0; i < menuLen; i++)
-            if (menu[i] == choseDrink)
-            {
-                if (prices[i] <= cash)
-                {
+            if (menu[i] == choseDrink) {
+                if (prices[i] <= cash) {
                     state = CHECK;
                     cash = cash - prices[i];
                     isEnough = 1;
-                }
-                else
-                {
+                } else {
                     cout << "not enough money" << endl;
                     state = ACCEPT;
                 }
             }
-    }
-    else
+    } else
         cout << "Error" << endl;
 }
-void Automata::cancel()
-{
+void Automata::cancel() {
     state = WAIT;
 }
-void Automata::cook()
-{
-    if (state == CHECK && isEnough == 1)
-    {
+void Automata::cook() {
+    if (state == CHECK && isEnough == 1) {
         state = COOK;
         cout << "PSHHHH" << endl;
-    }
-    else
+    } else
         cout << "Error" << endl;
 }
-void Automata::finish()
-{
-    if (state == COOK)
-    {
+void Automata::finish() {
+    if (state == COOK) {
         state = WAIT;
-    }
-    else
+    } else
         cout << "Error" << endl;
 }
