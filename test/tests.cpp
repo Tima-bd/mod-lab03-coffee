9764caf79e2ab1dd66d0037fb4333dec008b03f1
Tima-bd <100956970+Tima-bd@users.noreply.github.com>
2022-03-28 16:14:45 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,28 +1,25 @@
 // Copyright 2022 GHA Test Team
-
+// Copyright 2022 UNN-IASR
 #include <gtest/gtest.h>
 #include "Automata.h"
 
-TEST(task1, operation)
-{
-	Automata coffee;
-	coffee.state = OFF;
-	coffee.on();
-	States ok = coffee.get_state();
-	EXPECT_EQ(Wait, ok);
+TEST(task1, operation) {
+    Automata coffee;
+    coffee.state = OFF;
+    coffee.on();
+    States ok = coffee.get_state();
+    EXPECT_EQ(Wait, ok);
 }
-TEST(task2, operation)
-{
-	Automata coffee;
-	coffee.state = COOK;
-	coffee.on();
-	States ok = coffee.get_state();
-	EXPECT_EQ(COOK, ok);
+TEST(task2, operation) {
+    Automata coffee;
+    coffee.state = COOK;
+    coffee.on();
+    States ok = coffee.get_state();
+    EXPECT_EQ(COOK, ok);
 }
-TEST(task3, operation)
-{
-	Automata coffee;
-	coffee.state = CHECK;
-	coffee.check();
-	EXPECT_EQ(isEnough, 1);
+TEST(task3, operation) {
+    Automata coffee;
+    coffee.state = CHECK;
+    coffee.check();
+    EXPECT_EQ(isEnough, 1);
 }
